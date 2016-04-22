2273e671eb30a6001586e330d3549eaaf5f716e0
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2016-04-23 01:09:15 +0500

    [Analytics] Viewcontroller added

---
+++ Rabbitmail/AnalyticsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AnalyticsViewController.swift
+//  Rabbitmail
+//
+//  Created by Shumail Mohy ud Din on 4/23/16.
+//  Copyright © 2016 Rabbitmail. All rights reserved.
+//
+
+import UIKit
+
+class AnalyticsViewController: UIViewController {
+
+    @IBOutlet var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
